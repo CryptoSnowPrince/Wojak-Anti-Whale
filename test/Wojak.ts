6aac0f0038fe185dc6433ed13a443996796abd8a
CryptoSnowPrince <topdirector2017@gmail.com>
2023-05-07 19:00:05 +0200

    update

--- test/Wojak.ts
+++ test/Wojak.ts
@@ -3,9 +3,11 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import RouterABI from "@uniswap/v2-periphery/build/IUniswapV2Router02.json";
+import FactoryABI from "@uniswap/v2-periphery/build/IUniswapV2Factory.json";
 import WETH_ABI from "@uniswap/v2-periphery/build/WETH9.json";
 
 const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+const UNISWAP_FACTORY_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 
 describe("Wojak", function () {
@@ -15,17 +17,20 @@ describe("Wojak", function () {
   async function deployWojak() {
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, user1, user2, user3, otherAccount] = await ethers.getSigners();
 
     const Wojak = await ethers.getContractFactory("Wojak");
     const wojak = await Wojak.deploy();
 
-    return { wojak, owner, otherAccount };
+    const router = new ethers.Contract(UNISWAP_ROUTER_ADDRESS, RouterABI.abi, owner)
+    const factory = new ethers.Contract(UNISWAP_FACTORY_ADDRESS, FactoryABI.abi, owner)
+    const weth = new ethers.Contract(WETH_ADDRESS, WETH_ABI.abi, owner)
+    return { wojak, router, factory, weth, owner, user1, user2, user3, otherAccount };
   }
 
   describe("Deployment", function () {
     it("Should set the right marketingWallet", async function () {
-      const { wojak, owner, otherAccount } = await loadFixture(deployWojak);
+      const { wojak, router, factory, weth, owner, otherAccount } = await loadFixture(deployWojak);
       console.log("wojak address:", wojak.address)
       console.log("owner address:", owner.address)
       console.log("otherAccount address:", otherAccount.address)
@@ -34,6 +39,8 @@ describe("Wojak", function () {
 
       console.log("Wojak token Info")
       console.log("wojak decimals:", await wojak.decimals())
+      console.log("wojak name:", await wojak.name())
+      console.log("wojak symbol:", await wojak.symbol())
       console.log("wojak totalSupply:", await wojak.totalSupply())
       console.log("wojak buyTax:", await wojak.buyTax())
       console.log("wojak sellTax:", await wojak.sellTax())
@@ -44,32 +51,54 @@ describe("Wojak", function () {
       console.log("wojak whaleSellLimitPercent:", await wojak.whaleSellLimitPercent())
       console.log("wojak isCoolDownMode:", await wojak.isCoolDownMode())
       console.log("wojak owner:", await wojak.owner())
+
+      console.log('user list that except from fee')
+      const marketingWallet = await wojak.marketingWallet()
+      const listOfExcludedFromFee = [owner.address, marketingWallet, wojak.address]
+      console.log(listOfExcludedFromFee)
+
+      console.log('user list that anti whale')
+      const pair = await wojak.uniswapV2Pair()
+      const whiteList = [owner.address, wojak.address, pair]
+      console.log(whiteList)
+
+      console.log("Router Info")
+      console.log("Router address:", router.address)
+      console.log("Router factory:", await router.factory())
+      console.log("Router WETH:", await router.WETH())
+
+      console.log("Factory Info")
+      console.log("Factory address:", factory.address)
+      console.log("Factory factory:", await factory.allPairsLength())
+      console.log("Factory feeTo:", await factory.feeTo())
+      console.log("Factory feeToSetter:", await factory.feeToSetter())
+
+      console.log("WETH Info")
+      console.log("WETH address:", weth.address)
+      console.log("WETH name:", await weth.name())
+      console.log("WETH symbol:", await weth.symbol())
     });
+  });
 
-    // it("Should set the right owner", async function () {
-    //   const { lock, owner } = await loadFixture(deployWojak);
+  describe("AddLiquidity, Swap, RemoveLiquidity", function () {
+    it("AddLiquidity", async function () {
+      const { wojak, router, factory, weth, owner, user1, user2, user3, otherAccount } = await loadFixture(deployWojak);
 
-    //   expect(await lock.owner()).to.equal(owner.address);
-    // });
+      console.log("before balance of Wojak: ", ethers.utils.formatEther(await wojak.balanceOf(owner.address)), "Wojak")
+      console.log("before balance of ETH: ", ethers.utils.formatEther(await owner.getBalance()), "ETH")
+      await wojak.approve(router.address, ethers.utils.parseEther("10000000"))
 
-    // it("Should receive and store the funds to lock", async function () {
-    //   const { lock, lockedAmount } = await loadFixture(
-    //     deployWojak
-    //   );
+      await router.addLiquidityETH(wojak.address, ethers.utils.parseEther("5000000"), 0,0, owner.address, 10000000000, {value: ethers.utils.parseEther("500")})
 
-    //   expect(await ethers.provider.getBalance(lock.address)).to.equal(
-    //     lockedAmount
-    //   );
-    // });
+      console.log("after balance of Wojak: ", ethers.utils.formatEther(await wojak.balanceOf(owner.address)), "Wojak")
+      console.log("after balance of ETH: ", ethers.utils.formatEther(await owner.getBalance()), "ETH")
 
-    // it("Should fail if the unlockTime is not in the future", async function () {
-    //   // We don't use the fixture here because we want a different deployment
-    //   const latestTime = await time.latest();
-    //   const Lock = await ethers.getContractFactory("Lock");
-    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //     "Unlock time should be in the future"
-    //   );
-    // });
+      await wojak.configureExempted([user1.address], true)
+      await wojak.configureExempted([user2.address], false)
+
+      await wojak.transfer(user1.address, ethers.utils.parseEther("1000000"))
+      await wojak.transfer(user2.address, ethers.utils.parseEther("1000000"))
+    });
   });
 
   // describe("Withdrawals", function () {
